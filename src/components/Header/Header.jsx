bb157bbde305fecea1f07f498680a5c2631375dc
FarHan949 <uddinfarhan949@gmail.com>
2024-09-04 18:47:22 +0600

    added Header Components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { CgProfile } from 'react-icons/cg';
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <nav className='flex flex-col md:flex-row justify-between items-center p-4'>
+            <h1 className='text-2xl md:text-4xl font-bold mb-4 md:mb-0'>
+                <span className='text-red-500'>TASTE</span>NEST
+            </h1>
+
+            <div className='flex flex-col md:flex-row list-none gap-4 md:gap-8 text-sm md:text-lg items-center justify-center'>
+                <a className='hover:bg-red-500 rounded-lg p-2' href="#Home">Home</a>
+                <a className='hover:bg-red-500 rounded-lg p-2' href="#Recipe">Recipe</a>
+                <a className='hover:bg-red-500 rounded-lg p-2' href="#About">About</a>
+                <a className='hover:bg-red-500 rounded-lg p-2' href="#Email">Email</a>
+            </div>
+
+            <div className='flex items-center gap-4 md:gap-8 mt-4 md:mt-0'>
+                <div className='relative'>
+                    <input 
+                        className='p-2 pl-10 rounded-xl bg-slate-200 w-full md:w-auto focus:outline-none focus:ring-2 focus:ring-red-500'
+                        type="search" 
+                        placeholder='Search...'
+                    />
+                    <FaSearch className='absolute left-3 top-1/2 transform -translate-y-1/2 text-slate-700' />
+                </div>
+                <CgProfile className='text-red-400 text-2xl md:text-3xl' />
+            </div>
+        </nav>
     );
 };
 
