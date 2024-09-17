e460b4c36f224e95fcfef4c3c5bfa4573899114e
FarHan949 <uddinfarhan949@gmail.com>
2024-09-17 15:51:56 +0600

    upload fake data img

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { CgProfile } from 'react-icons/cg';
+import { FiMenu  } from 'react-icons/fi';
+import { IoMdClose } from "react-icons/io";
+
+
 
 const Header = () => {
+
+    const [open, setOpen] = useState(false)
+
+
     return (
         <nav className='flex flex-col md:flex-row justify-between items-center p-4 border-b-2 border-red-300'>
+           
+           <div className='md:hidden' onClick={()=>setOpen(!open)}>
+            {
+                open === true ? 
+                  <IoMdClose className='text-2xl'></IoMdClose>
+                : <FiMenu className='text-2xl '></FiMenu> 
+            }
+          </div>
+           
+
             <h1 className='text-2xl md:text-4xl font-bold mb-4 md:mb-0'>
                 <span className='text-red-500'>TASTE</span>NEST
             </h1>
 
-            <div className='flex flex-col md:flex-row list-none gap-4 md:gap-8 text-base md:text-lg items-center justify-center'>
+            <div className={`md:flex md:flex-row sm:flex-none list-none gap-4 md:gap-8 text-base md:text-lg items-center justify-center
+            ${open ? '' : 'hidden'}`}>
                 <a className='hover:bg-red-500 rounded-lg p-2' href="#Home">Home</a>
                 <a className='hover:bg-red-500 rounded-lg p-2' href="#Recipe">Recipe</a>
                 <a className='hover:bg-red-500 rounded-lg p-2' href="#About">About</a>
